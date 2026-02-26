999e209635b00e5c5d5ebf93b31aa760115ca1d3
sujal-lukhi01 <sujal.lukhi@pedalsup.com>
2026-02-26 15:34:06 +0530

    update footer contact information

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -76,18 +76,17 @@ const Footer = () => {
                 <div>
                     <h2 className='text-base font-medium text-gray-800 uppercase'>Contact</h2>
                     <ul className='mt-3 flex flex-col gap-1.5'>
-                        <li>1234 LJ University</li>
+                        <li>SAL Engineerng & Technical Institute</li>
                         <li>Ahmedabad,Gujrat</li>
-                        <li>+123456789</li>
-                        <li>info@ljku.com</li>
+                        <li>+91 1234567890</li>
+                        <li>221260116017setiit@gmail.com</li>
                     </ul>
                 </div>
 
                 </motion.div>
                 
 
-                  
-                
+
 
             </motion.div>
             
